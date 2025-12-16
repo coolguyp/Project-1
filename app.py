e858de3167aa576fbf31782470f0e9785e91aa59
pranam <pravangparmar19@gmaul.com>
2025-12-16 17:50:36 +0530

    change the message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "CI/CD with Docker and Kubernetes Working!"
+    return "CI/CD with Docker and Kubernetes Working well!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
