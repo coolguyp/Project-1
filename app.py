c9c96f6a674095a55d2fff1dd6606198d30ed368
PARMAR PRANAM <161856663+coolguyp@users.noreply.github.com>
2025-12-16 10:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "CI/CD with Docker and Kubernetes Working!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
